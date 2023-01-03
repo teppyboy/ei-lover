3e63867a28887e77ca1a668b09b400cb1cf6b229
tretrauit <tretrauit@gmail.com>
2023-01-04 01:19:50 +0700

    constants: set to unknown if we can't find git commit

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ import { exec } from 'child_process'
 
 const _exec = promisify(exec)
 
-const COMMIT: string = (await _exec('git rev-parse --short HEAD')).stdout.trim()
+const COMMIT: string = (await _exec('git rev-parse --short HEAD')).stdout.trim() ?? 'unknown'
 const VERSION: string = '0.0.1'
 const HOMESERVER: string = 'https://matrix-client.matrix.org'
 const PREFIX: string = '!'
