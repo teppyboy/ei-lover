961f9b7044b37e0ae517e1a4226e2bb0332011d2
tretrauit <tretrauit@gmail.com>
2023-01-04 17:20:23 +0700

    constants: add dirty status

--- src/constants.ts
+++ src/constants.ts
@@ -4,8 +4,9 @@ import { exec } from 'child_process'
 const _exec = promisify(exec)
 
 const COMMIT: string = (await _exec('git rev-parse --short HEAD')).stdout.trim() ?? 'unknown'
+const GIT_DIRTY: boolean = (await _exec('git status -s')).stdout.trim() === '' ? false : true
 const VERSION: string = '0.0.1'
 const HOMESERVER: string = 'https://matrix-client.matrix.org'
 const PREFIX: string = '!'
 
-export { COMMIT, VERSION, HOMESERVER, PREFIX }
+export { COMMIT, VERSION, HOMESERVER, PREFIX, GIT_DIRTY }
