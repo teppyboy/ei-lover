bbe4ad262b2974237b27f011742e6ec449bc6a11
tretrauit <tretrauit@gmail.com>
2023-05-16 21:48:17 +0700

    fix: spawn sudo in Windows
    Windows doesn't have sudo so we have to use gsudo instead.

--- src/helpers.ts
+++ src/helpers.ts
@@ -3,7 +3,16 @@ import { promisify } from 'util'
 import { userInfo } from 'os'
 
 const sleep = promisify(setTimeout)
+
+function spawnSudoNT(command: string, args: string[], ...rest: any[]) {
+    // You need "gsudo" for this to work.
+    return spawn('sudo', [command, ...args], ...rest)
+}
+
 function spawnSudo(command: string, args: string[], ...rest: any[]) {
+    if (process.platform == 'win32') {
+        return spawnSudoNT(command, args, ...rest)
+    }
     if (userInfo().uid == 0 && !process.env.SUDO_USER) {
         // Just spawn and return normally because we're already superuser
         return spawn((command = command), (args = args), ...rest)
